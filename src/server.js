d504208ad8881127c90b1145a72fc4b504040eb8
Gustavo Armagno <gustavo.armagno@moove-it.com>
2018-10-25 19:22:37 -0300

    Return {} if tea === -1

--- src/server.js
+++ src/server.js
@@ -9,7 +9,12 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => res.send({ name: 'Hello World!' }));
 
 app.get('/nextBus/:idLinea/:idParada', (req, res) => {
-  res.send( graphFactory.calculateETA({lineId: req.params.idLinea, stopId: req.params.idParada}) );
+  const result = graphFactory.calculateETA({lineId: req.params.idLinea, stopId: req.params.idParada});
+  if (result.tea === -1) {
+    res.send({});
+  } else {
+    res.send(result);
+  }
 });
 
 app.post('/callback', (req, res) => {
